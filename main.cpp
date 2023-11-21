c61be70799e940523a45e65295e6af87dce10895
gzhutaishuai <107551607+gzhutaishuai@users.noreply.github.com>
2023-11-21 15:53:34 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,56 @@
 #include "MyShader.h"
 
 
+/// <summary>
+/// Bresham算法画直线
+/// </summary>
+/// <param name="p0"></param>
+/// <param name="p1"></param>
+/// <param name="image"></param>
+/// <param name="color"></param>
+void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color)
+{
+    bool translate=false;
+    //如果dy>dx,也就是斜率大于1，那么关于应该让直线关于y=x对称交换
+    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y))
+    {
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
+        translate = true;
+    }
+    if (p0.x > p1.x)
+    {
+        std::swap(p0.x, p1.x);
+        std::swap(p0.y, p1.y);
+    }
+
+    int dx =p1.x - p0.x;
+    int dy = p1.y - p0.y;
+
+    int derror2 = 2*std::abs(p1.y-p0.y);
+
+    int error2 = 0;
+
+    int y = p0.y;
+    for (int x = p0.x; x <= p1.x; x++)
+    {
+        if (translate)
+        {
+            image.set(y, x, color);
+        }
+        else
+        {
+            image.set(x, y, color);
+        }
+        error2 += derror2;
+        if (error2-dx>0)
+        {
+            y += (p1.y > p0.y ? 1 : -1);
+            error2 -= dx * 2;
+        }
+    }
+}
+
 Model* model = NULL;//加载模型
 const int width = 800;//屏宽
 const int height = 800;//屏高
